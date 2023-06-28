164d7ae6e2b91bef4782573c5b1b0b4199d43784
JosipHorvat <josiph988@gmail.com>
2023-06-28 09:54:31 +0100

    Player can move in air, 2D Material so player don't stick to walls

--- Assets/PlayerAirState.cs
+++ Assets/PlayerAirState.cs
@@ -24,5 +24,8 @@ public class PlayerAirState : PlayerState
 
         if (player.IsGroundDetected())
             stateMachine.ChangeState(player.idleState);
+
+        if (xInput != 0)
+            player.SetVelocity(player.moveSpeed * .8f * xInput, rb.velocity.y);
     }
 }
