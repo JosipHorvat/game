ed9917229f2d60e220a12aff21e618de7083b7c7
JosipHorvat <josiph988@gmail.com>
2023-06-29 06:02:10 +0100

    Player wall Jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -40,6 +40,7 @@ public class Player : MonoBehaviour
     public PlayerAirState airState { get; private set; }
     public PlayerDashState dashState { get; private set; }
     public PlayerWallSlideState wallSlide { get; private set; }
+    public PlayerWallJumpState wallJump { get; private set; }
     #endregion
     private void Awake()
     {
@@ -51,6 +52,7 @@ public class Player : MonoBehaviour
         airState  = new PlayerAirState(this, stateMachine, "Jump");
         dashState = new PlayerDashState(this, stateMachine, "Dash");
         wallSlide = new PlayerWallSlideState(this, stateMachine, "WallSlide");
+        wallJump = new PlayerWallJumpState(this, stateMachine, "Jump");
     }
 
     private void Start()
